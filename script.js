e5fc8638624fd707d7bc6145b56d4bfc0c6521f7
dima-kulikov <skaizy44@gmail.com>
2021-10-19 15:54:14 +0300

    в
    в

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ new Vue({
     data: {
       message: 'Hello Vue!',
       id:'10',
-      status: 2,
+      status: 3,
       isActive: true,
       margin: 100,
       with: 400
 
+    }
+  })
+  new Vue({
+    el:'#second-vue',
+    data: {
+      message: 'компонент работает',
+      list: ['один', 'два', 'три'],
+      user: [
+        {id: 1, name: 'Дима'},
+        {id: 2, name: 'Леся'}
+      ]
     }
   })
