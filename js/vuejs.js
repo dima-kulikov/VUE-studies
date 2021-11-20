fa053be32bd17e9e37dfed30f80fcae25021812b
Dima <skaizy44@gmail.com>
2021-11-20 19:59:05 +0200

    22
    22

--- js/vuejs.js
+++ js/vuejs.js
@@ -1,8 +1,27 @@
 let myVue = new Vue({
-    el:'#app',
-    data:{
-        print: "Hello my work Vue",
-        title: "прописан тайтл для текста"
-    }
+  el: '#app',
+  data: {
+    print: "Hello my work Vue",
+    title: "прописан тайтл для текста"
+  }
 })
 
+let app2 = new Vue({
+  el: '#app-2',
+  data: {
+    todos: [
+      {
+        text: 'Изучить JavaScript',
+        name: 'паша'
+      },
+      {
+        text: 'Изучить Vue',
+        name: 'миша'
+      },
+      {
+        text: 'Создать что-нибудь классное',
+        name: 'саша'
+      }
+    ]
+  }
+})
