f3505f1bfcbb53b982831143b724a2060d6f2101
Dima <skaizy44@gmail.com>
2021-11-20 22:10:38 +0200

    11
    123

--- js/vuejs.js
+++ js/vuejs.js
@@ -1,3 +1,32 @@
+let app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'при клике текст станет большим'
+  },
+  methods: {
+    upWords: function () {
+      this.message = this.message.toUpperCase();
+    },
+    downWords: function () {
+      this.message = this.message.toLowerCase();
+    }
+  },
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------
 let myVue = new Vue({
   el: '#app',
   data: {
@@ -5,22 +34,25 @@ let myVue = new Vue({
     title: "прописан тайтл для текста"
   }
 })
-
+// ---------------------------------------
 let app2 = new Vue({
   el: '#app-2',
   data: {
     todos: [
       {
         text: 'Изучить JavaScript',
-        name: 'паша'
+        name: 'паша',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Orc_grunt_by_Lucas_Salcedo.jpg/220px-Orc_grunt_by_Lucas_Salcedo.jpg'
       },
       {
         text: 'Изучить Vue',
-        name: 'миша'
+        name: 'миша',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Orc_grunt_by_Lucas_Salcedo.jpg/220px-Orc_grunt_by_Lucas_Salcedo.jpg'
       },
       {
         text: 'Создать что-нибудь классное',
-        name: 'саша'
+        name: 'саша',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Orc_grunt_by_Lucas_Salcedo.jpg/220px-Orc_grunt_by_Lucas_Salcedo.jpg'
       }
     ]
   }
