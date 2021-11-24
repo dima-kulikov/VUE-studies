ce1a5dac5b79d28c6bc0fc850793260611308df4
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2021-11-24 23:36:58 +0200

    24.11.2021
    24.11.2021

--- js/itproger.js
+++ js/itproger.js
@@ -1,3 +1,22 @@
+
+
+
+new Vue({
+    el: '#add-2',
+    data: {
+        value: 1,
+        doblValue: '2'
+    },
+    methods: {
+        increment(el) {
+            this.value = el
+            this.doblValue = el * 2
+
+        }
+    }
+});
+  
+
 new Vue({
     el: '#add-1',
     data: {
@@ -10,4 +29,6 @@ new Vue({
 
         }
     }
-})
+});
+
+
