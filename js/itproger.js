ce637d710871d519ff344b278fafe742ffc33d8e
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2021-11-29 22:50:09 +0200

    29.11.2021
    29.11.2021

--- js/itproger.js
+++ js/itproger.js
@@ -1,11 +1,22 @@
-
+new Vue({
+    el: "#add-3",
+    data: {
+        show: true,
+        cars : [
+            {car:"bmv", speed: 300},
+            {car:"mers", speed: 290},
+            {car:"ford", speed: 260},
+            {car:"reno", speed: 197}
+        ]
+    }
+});
 
 
 new Vue({
     el: '#add-2',
     data: {
         value: 1,
-        doblValue: '2'
+        doblValue: 2
     },
     methods: {
         increment(el) {
